51de325ed60c68421fa8be596701e30371ad01d5
e062257 <e062257@mastercard.com>
2016-06-09 13:19:31 +0530

    Authentication using PassportLocal Strategy

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,16 @@
+var users = require('../../app/controllers/users.server.controller'),
+       passport = require('passport');
+module.exports = function (app) {
+     app.route('/signup')
+        .get(users.renderSignup)
+        .post(users.signup);
+     app.route('/signin')
+        .get(users.renderSignin)
+        .post(passport.authenticate('local', {
+          successRedirect: '/bookmeeting',
+          failureRedirect: '/signin',
+          failureFlash: true,
+        }));
+     app.get('/bookmeeting', users.bookmeeting);
+     app.get('/signout', users.signout);
+   };
